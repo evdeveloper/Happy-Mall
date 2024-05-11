e5dd7e52aec5df192a440bb59a353ce3f9ef53be
«Evgeny» <mushnikovz@mail.ru>
2024-05-11 08:19:15 +0300

    Правки home-page

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -15,6 +15,7 @@ class ControllerCommonHome extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
+		$data['class'] = str_replace('/', '-', $this->request->get['route'] ?? 'common-home');
 
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
