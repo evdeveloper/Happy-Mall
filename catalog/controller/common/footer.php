6b6ec8d879cb9fa2bf769abd86bc110d1bd02ef7
«Evgeny» <mushnikovz@mail.ru>
2024-06-23 10:52:13 +0300

    Вывод ссылок подкатегорий в выпадающее меню на главной

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -3,16 +3,69 @@ class ControllerCommonFooter extends Controller {
 	public function index() {
 		$this->load->language('common/footer');
 
+		$this->load->model('catalog/category');
+
 		$this->load->model('catalog/information');
 
 		$data['informations'] = array();
 
-		foreach ($this->model_catalog_information->getInformationsBottom() as $result) {
-			$data['informations'][] = array(
-				'title' => $result['title'],
-				'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
-			);
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(105);
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				// Level 2
+				$children_data = array();
+	
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+	
+				foreach ($children as $child) {
+	
+					// Level 3
+					$grandchildren_data = array();
+	
+					$grandchildren = $this->model_catalog_category->getCategories($child['category_id']);
+	
+					foreach ($grandchildren as $grandchild) {
+	
+						$grandchild_filter_data = array(
+							'filter_category_id'  => $grandchild['category_id'],
+							'filter_sub_category' => true
+						);
+	
+						$grandchildren_data[] = array(
+							'name'  => $grandchild['name'],
+							'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '_' . $grandchild['category_id']),
+						);
+					}
+	
+	
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+	
+					$children_data[] = array(
+						'name'  => $child['name'],
+						'image' => $category['image'],
+						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+						'children' => $grandchildren_data,
+					);
+				}
+	
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],             
+					'image'    => $category['image'],     
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id']),
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+				);
+			}
 		}
+		
+		
 
 		$data['home'] = $this->url->link('common/home');
 		$data['faq'] = $this->url->link('information/faq');
